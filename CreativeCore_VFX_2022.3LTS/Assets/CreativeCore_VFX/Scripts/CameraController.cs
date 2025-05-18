9bd79e3e9e558ce58a94474669b839b26679db2a
Jane <huangdonghao1997@gmail.com>
2025-05-18 16:29:46 +0800

    initial commit Project 'CreativeCore_VFX_2022.3LTS'

---
+++ CreativeCore_VFX_2022.3LTS/Assets/CreativeCore_VFX/Scripts/CameraController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float moveSpeed = 5f; // 移动速度
+    public float mouseSensitivity = 2f; // 鼠标灵敏度
+
+    private float rotationX = 0f;
+    private float rotationY = 0f;
+    private Vector3 initialRotation; // 初始旋转角度
+
+    void Start()
+    {
+        // 记录初始旋转角度
+        initialRotation = transform.localEulerAngles;
+        rotationX = initialRotation.x;
+        rotationY = initialRotation.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // WASD 控制移动
+        float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime; // A/D 或 左/右
+        float moveZ = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;   // W/S 或 上/下
+        transform.Translate(new Vector3(moveX, 0, moveZ));
+
+        // 按住鼠标右键时控制镜头旋转
+        if (Input.GetMouseButton(1)) // 鼠标右键
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+            rotationX -= mouseY;
+            rotationY += mouseX;
+
+            // 限制上下旋转角度
+            rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+
+            // 应用旋转时加上初始旋转角度
+            transform.localEulerAngles = new Vector3(rotationX, rotationY, 0f);
+        }
+    }
+}
