2d72347d68ec0d5bde61472b66b100856cb02e92
Jane <huangdonghao1997@gmail.com>
2025-05-18 22:50:32 +0800

    feat: 完善下雨粒子特效&添加雨滴材质&完善镜头控制脚本

--- CreativeCore_VFX_2022.3LTS/Assets/CreativeCore_VFX/Scripts/CameraController.cs
+++ CreativeCore_VFX_2022.3LTS/Assets/CreativeCore_VFX/Scripts/CameraController.cs
@@ -7,9 +7,16 @@ public class CameraController : MonoBehaviour
     public float moveSpeed = 5f; // 移动速度
     public float mouseSensitivity = 2f; // 鼠标灵敏度
 
+    public float maxSpeed = 20f; // 最大速度
+
     private float rotationX = 0f;
     private float rotationY = 0f;
     private Vector3 initialRotation; // 初始旋转角度
+
+    // horizontal增加量
+    private float verticalDeltaTimeIncrement = 0f;
+
+
 
     void Start()
     {
@@ -22,9 +29,20 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float vertical = Input.GetAxis("Vertical"); // W/S 或 上/下
+        if (vertical != 0)
+        {
+            verticalDeltaTimeIncrement += Time.deltaTime;
+        }
+        else
+        {
+            verticalDeltaTimeIncrement = 0f; // 重置增量
+        }
+
         // WASD 控制移动
         float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime; // A/D 或 左/右
-        float moveZ = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;   // W/S 或 上/下
+        float moveSpeedZ = Mathf.Clamp(moveSpeed + (verticalDeltaTimeIncrement * 5), 0, maxSpeed); // 计算移动速度
+        float moveZ = vertical * moveSpeedZ * Time.deltaTime;   // W/S 或 上/下
         transform.Translate(new Vector3(moveX, 0, moveZ));
 
         // 按住鼠标右键时控制镜头旋转
